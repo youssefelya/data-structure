b34ddb54c9b0a84471a8b83dd30eccee346abce5
Youssef <youssefelyamani96@gmail.com>
2024-04-21 14:12:31 +0100

    fix: binary tree tests

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -5,25 +5,7 @@ public class BinaryTree implements Tree {
     TreeNode root;
 
     public TreeNode add(int val) {
-        if (root == null) {
-            root = new TreeNode(val);
-            return root;
-        }
-        return addRecursive(root, val);
-    }
-
-    private TreeNode addRecursive(TreeNode current, int val) {
-        if (current == null) {
-            return new TreeNode(val);
-        }
-
-        if (val < current.value) {
-            current.left = addRecursive(current.left, val);
-        } else {
-            current.right = addRecursive(current.right, val);
-        }
-
-        return current;
+        return null;
     }
 
     @Override
@@ -42,22 +24,8 @@ public class BinaryTree implements Tree {
     }
 
     public boolean isExist(int val) {
-        return isExistRecursive(root, val);
-    }
+        return false;
 
-    private boolean isExistRecursive(TreeNode current, int val) {
-        if (current == null) {
-            return false;
-        }
-        if (val == current.value) {
-            return true;
-        }
-
-        if (val < current.value) {
-            return isExistRecursive(current.left, val);
-        } else {
-            return isExistRecursive(current.right, val);
-        }
     }
 
 }

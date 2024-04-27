d2d650191f4aa3358ed568538ef07fe207899300
Youssef <youssefelyamani96@gmail.com>
2024-04-27 16:39:45 +0100

    feat: redblack tree

---
+++ src/RedBlackTree.java
@@ -0,0 +1,74 @@
+package src;
+
+public class RedBlackTree implements Tree {
+
+    private TreeNode root;
+
+    public TreeNode add(int val) {
+        return null;
+    }
+
+    @Override
+    public TreeNode remove(int val) {
+        return null;
+    }
+
+    @Override
+    public boolean isBinaryTree() {
+        return false;
+    }
+
+    @Override
+    public boolean isRedBlackTree() {
+        return false;
+    }
+
+    public boolean isExist(int val) {
+        return false;
+    }
+
+    private void leftRotate(TreeNode node) {
+
+    }
+
+    private void rightRotate(TreeNode node) {
+
+    }
+
+    private void fixInsertion(TreeNode node) {
+    }
+
+    private void fixDeletion(TreeNode node) {
+    }
+
+    private void recolor(TreeNode node) {
+    }
+
+    private TreeNode sibling(TreeNode node) {
+        return null;
+    }
+
+    private TreeNode uncle(TreeNode node) {
+        return null;
+    }
+
+    private TreeNode grandparent(TreeNode node) {
+        return null;
+
+    }
+
+    private void rotateCase(TreeNode node) {
+
+    }
+
+    // Method to fix double red violation
+    private void fixDoubleRed(TreeNode node) {
+
+    }
+
+    // Method to fix black height violation
+    private void fixBlackHeight(TreeNode node) {
+
+    }
+
+}
